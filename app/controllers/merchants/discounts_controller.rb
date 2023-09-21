b16737f5046d8c2bbda0d9bbd9c7613c29fe0d30
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-21 17:32:06 -0500

    finalization of user story 1 views and stories

---
+++ app/controllers/merchants/discounts_controller.rb
@@ -0,0 +1,11 @@
+class Merchants::DiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @discounts = @merchant.discounts
+  end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:discount_id])
+  end
+end
--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -2,20 +2,28 @@ require "rails_helper"
 
 RSpec.feature "the merchant discounts index page" do
   describe 'when visiting /merchants/merchant_id/invoices' do
-    xit 'US14 displays a list of merchant invoice with a button to each invoice show page' do
-      merchant = Merchant.create!(name: "Bracelets 'n Stuff")
-      item = merchant.items.create!(name: "Bracelet", description: "Shiny", unit_price: 100)
+    it 'US14 displays a list of merchant invoice with a button to each invoice show page' do
+      merchant_1 = Merchant.create!(name: "Bracelets 'n Stuff")
+      discount_1 = Discount.create!(percentage: 20, quantity: 10, merchant: merchant_1)
+      discount_2 = Discount.create!(percentage: 30, quantity: 15, merchant: merchant_1)
+      item = merchant_1.items.create!(name: "Bracelet", description: "Shiny", unit_price: 100)
       customer = Customer.create!(first_name: "Robert", last_name: "Redford")
       invoice = customer.invoices.create!(status: 0)
       invoice_item = InvoiceItem.create!(item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: 100, status: 0)
 
-      visit dashboard_merchants_path
+      visit dashboard_merchants_path(merchant_1.id)
       
-      expect(page).to have_link("My Discounts")
-      click_link("discounts")
-      expect(page).to have_current_path("/merchants/#{merchant.id}/discounts")
+      expect(page).to have_button("View Discounts")
+      click_button("View Discounts")
+      expect(page).to have_current_path("/merchants/#{merchant_1.id}/discounts")
       within ".bulk_discounts" do
-        expect(page).to have_content("/merchants/#{merchant.id}/discounts")
+        expect(page).to have_content("#{discount_1.percentage}% off #{discount_1.quantity} items")
+        expect(page).to have_content("#{discount_2.percentage}% off #{discount_2.quantity} items")
+        expect(page).to have_link("#{discount_1.percentage}% off #{discount_1.quantity} items")
+        expect(page).to have_link("#{discount_2.percentage}% off #{discount_2.quantity} items")
+        click_link("#{discount_2.percentage}% off #{discount_2.quantity} items")
+        expect(page).to have_current_path("/merchants/#{merchant_1.id}/discounts/#{discount_2.id}")
+
       end
     end
   end
