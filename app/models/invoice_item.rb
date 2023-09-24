973a6617d62841c94973422f7e78f44f6229daf6
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-23 21:14:30 -0500

    invoice item query work

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -25,11 +25,13 @@ class InvoiceItem < ApplicationRecord
   # end
   
   def available_discounts 
-    discounts.select('discounts.*, 
-    sum(quantity * unit_price) as total_price,
-    (sum(quantity * unit_price) * discounts.percentage / 100) as discount_total_off, 
-    sum(quantity * unit_price) - (sum(quantity * unit_price) * discounts.percentage / 100) as discounted_total')
-    .where('discounts.quantity <= ?', self.quantity)
+    self.discounts
+    .joins(:invoice_items)
+    .select('discounts.*, 
+    sum(invoice_items.quantity * invoice_items.unit_price) as total_price,
+    (sum(invoice_items.quantity * invoice_items.unit_price) * discounts.percentage / 100) as discount_total_off, 
+    sum(invoice_items.quantity * invoice_items.unit_price) - (sum(invoice_items.quantity * invoice_items.unit_price) * discounts.percentage / 100) as discounted_total')
+    .where('discounts.quantity <= ? AND invoice_items.id = ?', self.quantity, self.id)
     .group('discounts.id').order('discounts.percentage desc')
     .limit(1)
   end
