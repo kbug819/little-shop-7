b04b8c7bd7965161c57364955b2290e41a45bc07
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-23 21:14:03 -0500

    trial with associations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
   
   validates :percentage, presence: true
   validates :quantity, presence: true
