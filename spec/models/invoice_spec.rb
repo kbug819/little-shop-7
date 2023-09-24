d718b456514351d7c9c3e5ab6547340dc544a0a8
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-23 21:13:36 -0500

    invoice model spec testing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -13,7 +13,45 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of :status }
   end
 
+  describe '#available_discounts' do
+    it "shows only the highest percentage applicable discount and returns several calculations" do
+      customer_1 = Customer.create(first_name: "Joey", last_name:"One")
 
+      merchant_1 = Merchant.create(name: "merchant1")
+      merchant_2 = Merchant.create(name: "merchant2")
+
+      discount_1 = Discount.create(percentage: 20, quantity: 10, merchant: merchant_1)
+      discount_2 = Discount.create(percentage: 30, quantity: 15, merchant: merchant_1)
+      discount_3 = Discount.create(percentage: 15, quantity: 15, merchant: merchant_1)
+      item_1 = Item.create(name: "item1", description: "1", unit_price: 2145, merchant: merchant_1)
+      item_2 = Item.create(name: "item2", description: "1", unit_price: 2145, merchant: merchant_1)
+      item_3 = Item.create(name: "item2", description: "1", unit_price: 2145, merchant: merchant_2)
+      invoice_1 = Invoice.create(customer: customer_1, status: 0)
+      
+      invoice_item_1 = InvoiceItem.create(item: item_1, invoice: invoice_1, quantity: 5, unit_price: 100, status: 0)
+      invoice_item_2 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 10, unit_price: 1000, status: 1)
+      invoice_item_3 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 10, unit_price: 1000, status: 1)
+      # invoice_item_4 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 5, unit_price: 34343, status: 1)
+      invoice_item_5 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 12, unit_price: 100, status: 1)
+      # invoice_item_6 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 15, unit_price: 34343, status: 1)
+      # invoice_item_7 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 12, unit_price: 34343, status: 1)
+      # invoice_item_8 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 15, unit_price: 34343, status: 1)
+      # invoice_item_9 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 12, unit_price: 34343, status: 1)
+      # invoice_item_10 = InvoiceItem.create(item: item_2, invoice: invoice_1, quantity: 15, unit_price: 34343, status: 1)
+      # invoice_item_11 = InvoiceItem.create(item: item_3, invoice: invoice_1, quantity: 15, unit_price: 34343, status: 1)
+      
+
+
+      expect(invoice_item_3.available_discounts[0].total_price).to eq(10000)
+      expect(invoice_item_3.available_discounts[0].discount_total_off).to eq(2000)
+      expect(invoice_item_3.available_discounts[0].discounted_total).to eq(8000)
+
+      expect(invoice_item_5.available_discounts[0].total_price).to eq(1200)
+      expect(invoice_item_5.available_discounts[0].discount_total_off).to eq(240)
+      expect(invoice_item_5.available_discounts[0].discounted_total).to eq(960)
+      expect(invoice_1.discounted_revenue).to eq(17460)
+    end
+  end
 
   context 'standard set up' do
     before :each do
